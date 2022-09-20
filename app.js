4cbbb19ecddf5b58c73ca7b539eaff1f406a380a
RMS-Project <105136548+RMS-Project@users.noreply.github.com>
2022-09-19 21:31:00 -0300

    app

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Possui vávias dependencias do projeto.
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -7,20 +9,27 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// Importa o Express.
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+// Qual engine ele vai utilizar que no caso é o ejs.
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Pasta publica de arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Rotas do servidor
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Cria rotas em caso de acessos inválidos.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +47,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// Exportação da aplicação.
 module.exports = app;
